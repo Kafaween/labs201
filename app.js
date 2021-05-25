9f607aa659b5c0bb2943c1e89f73d1aedc398907
Kafaween <abod.kafaween@gmail.com>
2021-05-25 04:40:58 -0700

    editing

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
-let w=prompt("what is your weight (kg)")
-let h=prompt("what is your height(cm)")
-let a=prompt("whats your age " )
-let f=prompt("whats your activity factor (1.2 or 1.5 or 1.7)")
+'use strict'
 
-let bmr =10*w+6.25*h-5*a+5
-c=bmr*f
+const n =prompt("whats your name ?")
 
-alert("your bmr is " + bmr ) 
-alert (" your calories per day " +c )
+alert(" Welcome to my site " + n)
 
 
 
+
+alert("Answer these questions with Yes or No Pleas")
+let q1=prompt("do i like sports")
+let q2=prompt("is mansaf my fav meal")
+let q3 =prompt("do i watch movies")
+let q4 =prompt(" am i the best Dota 2 player  :)??")
+let q5 = prompt("is milkshake my fav drink")
+
+const arr = [q1,q2,q3,q4,q5];
+
+
+
+arr.forEach(myFunction);
+
+    function myFunction(x){
+    switch(x.toLowerCase()){
+        case "y":    
+        case "yes":
+            
+            //console.log("you are right")
+            alert("you are right")
+            break;   
+        case "n" :
+            
+        case "no" :
+            //console.log("you are wrong")
+            alert("you are wrong")
+            break;
+
+
+
+    }
+
+
+} 
+    
+alert("it was to see you "+ n)
 
 
